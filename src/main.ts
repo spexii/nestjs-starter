c704904e43253487401ba4608946e546df41df59
Pekka Piispanen <pekka.piispanen@futurice.com>
2022-09-13 14:54:21 +0300

    Added a log about application running url

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

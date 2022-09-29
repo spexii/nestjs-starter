920c3b2a2d30cb1721d274885ca37a5cad42eb01
Pekka Piispanen <pekka.piispanen@futurice.com>
2022-09-29 18:33:45 +0300

    Created a health endpoint to check if DB is up

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
